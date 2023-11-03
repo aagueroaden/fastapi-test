c5c0616481d04a250eef32dfa58953b465e87a2e
agustin.aguero <agustin.aguero@aden.org>
2023-11-03 15:03:07 -0300

    moving proyect to python3.10

---
+++ app/schemas/schemas.py
@@ -0,0 +1,21 @@
+# baseModel verity the type of data on wich the classes are instanciated
+from pydantic import BaseModel
+
+
+class AppSchema(BaseModel):
+    port: int
+    host: str
+
+
+class SalesForceSchema(BaseModel):
+    sf_client_id: str
+    sf_client_secret: str
+    sf_redirect_uri: str
+    sf_login_url: str
+    sf_user: str
+    sf_pass: str
+    sf_password: str
+
+
+class AuthSchema(BaseModel):
+    auth_salesforce_api_key: str
