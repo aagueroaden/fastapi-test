aaf6018f6a1e638df6c86b59735cb85e02bdbbfa
agustin.aguero <agustin.aguero@aden.org>
2023-11-03 17:15:31 -0300

    removing comments

--- app/salesforce/salesforce.py
+++ app/salesforce/salesforce.py
@@ -2,36 +2,19 @@ from simple_salesforce import Salesforce
 # from simple_salesforce import SalesforceLogin
 import requests
 from app.schemas.schemas import SalesForceSchema
+from fastapi import HTTPException
+
+
+token: str = ''
+url: str = ''
+
+# singleton
+instanceConnection: bool = False
 
 
 class SalesForceService:
 
-    # def __init__(
-    #         self,
-    #         # userId,
-    #         # orgId,
-    #         clientId,
-    #         clientSecret,
-    #         redirectUri,
-    #         loginUrl,
-    #         user,
-    #         password,
-
-    # ) -> None:
-    #     # self.userId: str = userId
-    #     # self.orgId: str = orgId
-    #     self.clientId: str = clientId
-    #     self.clientSecret: str = clientSecret
-    #     self.redirectUri: str = redirectUri
-    #     self.loginUrl: str = loginUrl
-    #     self.user: str = user
-    #     self.password: str = password
-
-    def __init__(
-            self,
-            settingsSF: SalesForceSchema
-
-    ) -> None:
+    def __init__(self, settingsSF: SalesForceSchema) -> None:
         # self.userId: str = userId
         # self.orgId: str = orgId
         self.clientId: str = settingsSF.sf_client_id
@@ -40,10 +23,11 @@ class SalesForceService:
         self.loginUrl: str = settingsSF.sf_login_url
         self.user: str = settingsSF.sf_user
         self.password: str = settingsSF.sf_password
-        self.connect()
+        if not instanceConnection:
+            self.connection: Salesforce = self._connect()
 
     # does not use any library
-    def connectPrimitive(self):
+    def _connectPrimitive(self):
         payload = {
             'client_id': self.clientId,
             'client_secret': self.clientSecret,
@@ -57,20 +41,21 @@ class SalesForceService:
         testing = response.json()
         print(testing['access_token'])
 
-    def connect(self):
-        sf = Salesforce(
-            username=self.user,
-            password=self.password,
-            consumer_key=self.clientId,
-            consumer_secret=self.clientSecret,
-            # instance_url=self.redirectUri,
-            domain='test',  # This parameter allows the connection to the sandbox
-        )
-        print(sf.session_id)
-        return {'session_id': sf.session_id}
-
-    def login(self):
-        pass
+    def _connect(self):
+        try:
+            sf_connection = Salesforce(
+                username=self.user,
+                password=self.password,
+                consumer_key=self.clientId,
+                consumer_secret=self.clientSecret,
+                # instance_url=self.redirectUri,
+                domain='test',  # This parameter allows the connection to the sandbox
+            )
+        except Exception | HTTPException as e:
+            print(f"Connection ERROR {e}")
+            print(f"cannot connect to {self.loginUrl}")
+        print(f"connected to {self.loginUrl}")
+        return sf_connection
 
     def executeQuery(self):
         pass
@@ -96,6 +81,12 @@ class SalesForceService:
     async def delete(self):
         pass
 
+    async def upsert(self):
+        pass
+
+    async def describle(self):
+        pass
+
     async def describeCache(self):
         pass
 
@@ -105,26 +96,5 @@ class SalesForceService:
     async def describeGlobalCacheClear(self):
         pass
 
-    async def clear(self):
+    async def clearCache(self):
         pass
-
-
-# if __name__ == '__main__':
-#     SF_CLIENT_ID = '3MVG9QBLg8QGkFerS4zTdoCnaDJSqgN5hEzYxe1HpQ25vk0JcEdNWtgvX6LBIxnzMWBYw8XpQMoLS9ldjZ1iP'
-#     SF_CLIENT_SECRET = 'AB6A92A56CC0C6C051C9305CEFA71733F36B9C5A647FBF923C740823837E9C68'
-#     SF_REDIRECT_URI = 'https://adenuniversity--admins3.sandbox.my.salesforce.com/services/oauth2/token'
-#     SF_LOGIN_URL = 'https://adenuniversity--admins3.sandbox.my.salesforce.com/'
-#     SF_USER = 'admin.salesforce@aden.org.admins3'
-#     SF_PASS = '4W7$kCDAwMr06MDQTSaFwwGuXRi8e4nKdfY'
-#     SF_PASSWORD = '4W7$kCDAwM'
-#     AUTH_SALESFORCE_API_KEY = 'eceebc33-e41b-4954-b639-86368773df77'
-#     salesforceservice = SalesForceService(
-#         clientId=SF_CLIENT_ID,
-#         clientSecret=SF_CLIENT_SECRET,
-#         redirectUri=SF_REDIRECT_URI,
-#         loginUrl=SF_LOGIN_URL,
-#         user=SF_USER,
-#         password=SF_PASS,
-#     )
-#     # salesforceservice.connect()
-#     # salesforceservice.anotherConnect()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
-from fastapi import FastAPI, Depends
-from typing import Annotated
+from fastapi import FastAPI
+
 from settings import envArgumentValidation, getEnvSettings
 
 from app.salesforce.salesforce import SalesForceService
@@ -18,22 +18,7 @@ async def root():
 @app.get("/salesforce")
 async def salesforce():
     service = SalesForceService(salesforce_settings)
-    print(service.connectPrimitive())
-    return service.connect()
-
-
-async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
-
-
-@app.get("/items/")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
+    return {'session_id': service.connection.session_id}
 
 
 if __name__ == '__main__':
